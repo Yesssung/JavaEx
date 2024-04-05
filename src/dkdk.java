44eaba690d56b2d4e5df164f0c5bf9b7569ce374
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-05 18:08:48 +0900

    24.04.05 학원

--- src/dkdk.java
+++ src/dkdk.java
@@ -1,19 +1,39 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class dkdk {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("번호를 입력하세요: \n 단: ");
 	
-		int num = scanner.nextInt();
 		
-		for(int i = 1; i <= 9; i++) {
-			System.out.println( num + "*" + i + "=" + (i*num) );
-			
-		}
-		System.out.println();
-		scanner.close();
+//		1.
+//		int a = 80;
+//		int b = 75;
+//		int c = 55;
+//		System.out.println((a+b+c)/3);
+//		
+//		2.
+//		String pin = "881120-1068234";
+//		String yyyyMMdd = "881120";
+//		String num = pin.substring(7);
+//		System.out.println(yyyyMMdd);
+//		System.out.println(num);
+		
+//		3.
+//		String pin="881120-1068234";
+//		System.out.println(pin.charAt(7) + "BOY");
+//		
+//		
+//		4.
+//		String a = "a:b:c:d";
+//		String b = a.replace(":", "#");
+//		System.out.println(b);
+//
+//		5.
+		ArrayList<Integer>myList = new ArrayList<>(Arrays.asList(1,3,5,4,2));
+		myList.sort(Comparator.reverseOrder());
+		System.out.println(myList);
 	}
 
 
