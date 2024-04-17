d495e3a89a883c7eaf24b25474933956c4480085
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-17 09:14:15 +0900

    24.04.17 학원

---
+++ src/com/javaex/network/InetAddressEx.java
@@ -0,0 +1,46 @@
+package com.javaex.network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressEx {
+	
+	public static void main(String[] args) {
+		// 내 PC의 로컬 IP
+		printLocalIP();
+		// 서버 IP(naver, google)
+		printServerIP("www.naver.com");
+		printServerIP("www.google.com");
+	}
+	
+	private static void printServerIP(String hostName) {
+		try {
+			InetAddress[] hosts = InetAddress.getAllByName(hostName);
+				for(InetAddress remote: hosts) {
+					System.out.println(hostName + " -> IP Address: " + remote.getHostAddress());
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+		}
+	}
+
+
+
+
+	
+	private static void printLocalIP() {
+		// Local loop back: 127.0.0.1
+		// PC : IP 
+		try {
+		InetAddress local = InetAddress.getLocalHost();
+		System.out.println("IP Address: " + local.getHostAddress());
+		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+
+}
